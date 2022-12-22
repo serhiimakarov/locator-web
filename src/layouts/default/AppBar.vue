1881c55e63480f516ac2e3344be605fc88280d19
Serhii Makarov <makarovserg2@gmail.com>
2022-12-22 20:21:26 +0200

    #796995 [WEB] Implement basic layout
    replace element ui with vuetify

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-4" />
+
+      Base Preset
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
